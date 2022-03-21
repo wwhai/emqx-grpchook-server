118cb7522965daa9312c8ceb573766a4a6e8c4b1
wwhai <751957846@qq.com>
2022-03-21 23:57:26 +0800

    dev: update proto; add more log tag

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 set -e
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
+
 echo ">>> Generate grpchook Proto."
 protoc -I ./grpchook --go_out ./grpchook --go_opt paths=source_relative \
     --go-grpc_out=./grpchook --go-grpc_opt paths=source_relative \
--- main.go
+++ main.go
@@ -17,57 +17,49 @@ type rpc_hook_server struct {
 }
 
 func (s *rpc_hook_server) ClientConnect(ctx context.Context, request *grpchook.ConnectRequest) (response *grpchook.ConnectResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientConnect========>", request)
 	response = new(grpchook.ConnectResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientConnack(ctx context.Context, request *grpchook.ConnackRequest) (response *grpchook.ConnackResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientConnack========>", request)
 	response = new(grpchook.ConnackResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientConnected(ctx context.Context, request *grpchook.ConnectedRequest) (response *grpchook.ConnectedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientConnected========>", request)
 	response = new(grpchook.ConnectedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientDisconnected(ctx context.Context, request *grpchook.DisconnectedRequest) (response *grpchook.DisconnectedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientDisconnected========>", request)
 	response = new(grpchook.DisconnectedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientAuthenticate(ctx context.Context, request *grpchook.AuthenticateRequest) (response *grpchook.AuthenticateResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientAuthenticate========>", request)
 	response = new(grpchook.AuthenticateResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientCheckAcl(ctx context.Context, request *grpchook.CheckAclRequest) (response *grpchook.CheckAclResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientCheckAcl========>", request)
 	response = new(grpchook.CheckAclResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientSubscribe(ctx context.Context, request *grpchook.SubscribeRequest) (response *grpchook.SubscribeResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientSubscribe========>", request)
 	response = new(grpchook.SubscribeResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) ClientUnsubscribe(ctx context.Context, request *grpchook.UnsubscribeRequest) (response *grpchook.UnsubscribeResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("ClientUnsubscribe========>", request)
 	response = new(grpchook.UnsubscribeResponse)
 	response.Result = true
 	return response, nil
@@ -75,29 +67,25 @@ func (s *rpc_hook_server) ClientUnsubscribe(ctx context.Context, request *grpcho
 
 // Session
 func (s *rpc_hook_server) SessionCreated(ctx context.Context, request *grpchook.SessionCreatedRequest) (response *grpchook.SessionCreatedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("SessionCreated========>", request)
 	response = new(grpchook.SessionCreatedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) SessionSubscribed(ctx context.Context, request *grpchook.SessionSubscribedRequest) (response *grpchook.SessionSubscribedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("SessionSubscribed========>", request)
 	response = new(grpchook.SessionSubscribedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) SessionUnsubscribed(ctx context.Context, request *grpchook.SessionUnsubscribedRequest) (response *grpchook.SessionUnsubscribedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("SessionUnsubscribed========>", request)
 	response = new(grpchook.SessionUnsubscribedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) SessionTerminated(ctx context.Context, request *grpchook.SessionTerminatedRequest) (response *grpchook.SessionTerminatedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("SessionTerminated========>", request)
 	response = new(grpchook.SessionTerminatedResponse)
 	response.Result = true
 	return response, nil
@@ -105,29 +93,25 @@ func (s *rpc_hook_server) SessionTerminated(ctx context.Context, request *grpcho
 
 // Message
 func (s *rpc_hook_server) MessagePublish(ctx context.Context, request *grpchook.MessagePublishRequest) (response *grpchook.MessagePublishResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("MessagePublish========>", request)
 	response = new(grpchook.MessagePublishResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) MessageDelivered(ctx context.Context, request *grpchook.MessageDeliveredRequest) (response *grpchook.MessageDeliveredResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("MessageDelivered========>", request)
 	response = new(grpchook.MessageDeliveredResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) MessageAcked(ctx context.Context, request *grpchook.MessageAckedRequest) (response *grpchook.MessageAckedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("MessageAcked========>", request)
 	response = new(grpchook.MessageAckedResponse)
 	response.Result = true
 	return response, nil
 }
 func (s *rpc_hook_server) MessageDropped(ctx context.Context, request *grpchook.MessageDroppedRequest) (response *grpchook.MessageDroppedResponse, err error) {
-
-	log.Debug("========>", request)
+	log.Debug("MessageDropped========>", request)
 	response = new(grpchook.MessageDroppedResponse)
 	response.Result = true
 	return response, nil
